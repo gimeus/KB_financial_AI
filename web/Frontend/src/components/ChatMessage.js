ec025e9ac21903271ec0d49b3431893ae50a9547
gimeus <gimeus@naver.com>
2024-08-10 23:56:12 +0900

    UI 수정

--- web/Frontend/src/components/ChatMessage.js
+++ web/Frontend/src/components/ChatMessage.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './ChatMessage.css';
-import BotLogo from '../assets/icons/KB.svg'; // SVG 파일 경로
+import BotLogo from '../assets/icons/KB.svg';
+
+const formatTime = (time) => {
+  const period = time.includes('오후') || time.includes('PM') ? 'PM' : 'AM';
+
+  let [hour, minute] = time.match(/\d{1,2}:\d{2}/)[0].split(':');
+
+  hour = parseInt(hour, 10);
+
+  if (period === 'PM' && hour < 12) {
+    hour += 12;
+  } else if (period === 'AM' && hour === 12) {
+    hour = 0;
+  }
+
+  hour = hour % 12 || 12;
+
+  return `${hour}:${minute} ${period}`;
+};
 
 const ChatMessage = ({ type, children, time }) => {
   return (
@@ -10,12 +28,12 @@ const ChatMessage = ({ type, children, time }) => {
           <img src={BotLogo} alt="Bot Logo" className="message-bot-logo" />
           <div className="message-time-text">
             <span className="message-text-label">KB Bank</span>
-            <span className="message-time">{time}</span>
+            <span className="message-time">{formatTime(time)}</span>
           </div>
         </div>
       )}
       {type === 'user' && (
-        <div className="message-time message-time-user">{time}</div>
+        <div className="message-time message-time-user">{formatTime(time)}</div>
       )}
       <div
         className={`message ${
